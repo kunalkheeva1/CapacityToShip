5b734182b8a39cd908ab11666ac35d572416a1bc
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-12 01:53:16 -0700

    created a method which returns the minimum number of weight required to be sent through ship in d Days

--- src/CapacityToShip.java
+++ src/CapacityToShip.java
@@ -1,5 +1,35 @@
 public class CapacityToShip {
+
+
+
+    public static int capacityToShipInDDays(int [] arr, int n, int d){
+        int low = 0;
+        int high =0;
+        for(int i= 0; i<n; i++){
+            high+= arr[i];
+            low = Math.max(low, arr[i]);
+        }
+
+        int ans =-1;
+        while(low<=high){
+            int mid = (low + high)/2;
+            if(check(arr, n, d, mid)){
+                ans = mid;
+                high =mid -1;
+            }
+            else{
+                low = mid+1;
+            }
+        }return ans;
+
+    }
+
+
+
+
+
+
     public static void main(String[] args) {
-        
+
     }
 }
